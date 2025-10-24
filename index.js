80044a786e91b82d4f9d255dc658ae691aca6e84
Almoghrabi3d <94754098+Almoghrabi3d@users.noreply.github.com>
2025-10-24 18:56:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const app = express();
+
+
 app.get("/Hello", (req, res) => {
     res.send("Hello World!");
 
@@ -8,6 +10,18 @@ app.get("/Hello", (req, res) => {
 
 app.get("/Goodbye", (req, res) => {
     res.send("Goodbye World!");
+});
+
+app.get("/name", (req, res) => {
+    res.send("My Name is Marwan Almoghrabi");
+});
+
+app.get("/age", (req, res) => {
+    res.send("My Age is 42");
+});
+
+app.post("/comment", (req, res) => {
+    res.send("Comment received");
 });
 
 app.listen(3000, () => {
